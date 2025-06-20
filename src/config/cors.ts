75e9b338d602a9b2ee051ee6cdca7aedc93d4389
TheFarawayDev <142858715+TheFarawayDev@users.noreply.github.com>
2025-06-20 12:05:14 -0400

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,13 +1,15 @@
-import { config } from "dotenv";
 import { cors } from "hono/cors";
+import { env } from "./env.js";
 
-config();
+const DEFAULT_ALLOWED_ORIGINS = ["http://localhost:4000", "*"];
 
-const corsConfig = cors({
-  allowMethods: ["GET"],
-  maxAge: 600,
-  credentials: true,
-  origin: "*", // This is the important change!
+const allowedOrigins = env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
+    ? env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
+    : DEFAULT_ALLOWED_ORIGINS;
+
+export const corsConfig = cors({
+    allowMethods: ["GET"],
+    maxAge: 600,
+    credentials: true,
+    origin: allowedOrigins,
 });
-
-export default corsConfig;
