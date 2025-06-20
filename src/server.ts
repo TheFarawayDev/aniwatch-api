7e9e260c00e820bcae420e3c4974fcd200e67f31
TheFarawayDev <142858715+TheFarawayDev@users.noreply.github.com>
2025-06-20 12:04:19 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,72 +1,100 @@
 import https from "https";
-import { config } from "dotenv";
-
-import corsConfig from "./config/cors.js";
-import { ratelimit } from "./config/ratelimit.js";
-
-import {
-  cacheConfigSetter,
-  cacheControlMiddleware,
-} from "./middleware/cache.js";
-import { hianimeRouter } from "./routes/hianime.js";
 
 import { Hono } from "hono";
-import { logger } from "hono/logger";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 
-import pkgJson from "../package.json" with { type: "json" };
+import { log } from "./config/logger.js";
+import { corsConfig } from "./config/cors.js";
+import { ratelimit } from "./config/ratelimit.js";
+import { execGracefulShutdown } from "./utils.js";
+import { DeploymentEnv, env, SERVERLESS_ENVIRONMENTS } from "./config/env.js";
 import { errorHandler, notFoundHandler } from "./config/errorHandler.js";
-import type { AniwatchAPIVariables } from "./config/variables.js";
+import type { ServerContext } from "./config/context.js";
 
-config();
+import { hianimeRouter } from "./routes/hianime.js";
+import { logging } from "./middleware/logging.js";
+import { cacheConfigSetter, cacheControl } from "./middleware/cache.js";
 
+import pkgJson from "../package.json" with { type: "json" };
+
+//
 const BASE_PATH = "/api/v2" as const;
-const PORT: number = Number(process.env.ANIWATCH_API_PORT) || 4000;
-const ANIWATCH_API_HOSTNAME = process.env?.ANIWATCH_API_HOSTNAME;
 
-const app = new Hono<{ Variables: AniwatchAPIVariables }>();
+const app = new Hono<ServerContext>();
 
-app.use(logger());
+app.use(logging);
 app.use(corsConfig);
-app.use(cacheControlMiddleware);
+app.use(cacheControl);
 
-// CAUTION: For personal deployments, "refrain" from having an env
-// named "ANIWATCH_API_HOSTNAME". You may face rate limitting
-// or other issues if you do.
-const ISNT_PERSONAL_DEPLOYMENT = Boolean(ANIWATCH_API_HOSTNAME);
-if (ISNT_PERSONAL_DEPLOYMENT) {
-  app.use(ratelimit);
+/*
+    CAUTION: 
+    Having the "ANIWATCH_API_HOSTNAME" env will
+    enable rate limitting for the deployment.
+    WARNING:
+    If you are using any serverless environment, you must set the
+    "ANIWATCH_API_DEPLOYMENT_ENV" to that environment's name, 
+    otherwise you may face issues.
+*/
+const isPersonalDeployment = Boolean(env.ANIWATCH_API_HOSTNAME);
+if (isPersonalDeployment) {
+    app.use(ratelimit);
 }
 
+// if (env.ANIWATCH_API_DEPLOYMENT_ENV === DeploymentEnv.NODEJS) {
 app.use("/", serveStatic({ root: "public" }));
+// }
+
 app.get("/health", (c) => c.text("daijoubu", { status: 200 }));
 app.get("/v", async (c) =>
-  c.text(
-    `v${"version" in pkgJson && pkgJson?.version ? pkgJson.version : "-1"}`
-  )
+    c.text(
+        `aniwatch-api: v${"version" in pkgJson && pkgJson?.version ? pkgJson.version : "-1"}\n` +
+            `aniwatch-package: v${"dependencies" in pkgJson && pkgJson?.dependencies?.aniwatch ? pkgJson?.dependencies?.aniwatch : "-1"}`
+    )
 );
 
 app.use(cacheConfigSetter(BASE_PATH.length));
 
 app.basePath(BASE_PATH).route("/hianime", hianimeRouter);
-app
-  .basePath(BASE_PATH)
-  .get("/anicrush", (c) => c.text("Anicrush could be implemented in future."));
+app.basePath(BASE_PATH).get("/anicrush", (c) =>
+    c.text("Anicrush could be implemented in future.")
+);
 
 app.notFound(notFoundHandler);
 app.onError(errorHandler);
 
-// NOTE: this env is "required" for vercel deployments
-if (!Boolean(process.env?.ANIWATCH_API_VERCEL_DEPLOYMENT)) {
-  serve({
-    port: PORT,
-    fetch: app.fetch,
-  }).addListener("listening", () =>
-    console.info(
-      "\x1b[1;36m" + `aniwatch-api at http://localhost:${PORT}` + "\x1b[0m"
-    )
-  );
-}
- 
- export default app;
+//
+(function () {
+    /*
+        NOTE:
+        "ANIWATCH_API_DEPLOYMENT_MODE" env must be set to
+        its supported name for serverless deployments
+        Eg: "vercel" for vercel deployments
+    */
+    if (SERVERLESS_ENVIRONMENTS.includes(env.ANIWATCH_API_DEPLOYMENT_ENV)) {
+        return;
+    }
+
+    const server = serve({
+        port: env.ANIWATCH_API_PORT,
+        fetch: app.fetch,
+    }).addListener("listening", () =>
+        log.info(
+            `aniwatch-api RUNNING at http://localhost:${env.ANIWATCH_API_PORT}`
+        )
+    );
+
+    process.on("SIGINT", () => execGracefulShutdown(server));
+    process.on("SIGTERM", () => execGracefulShutdown(server));
+    process.on("uncaughtException", (err) => {
+        log.error(`Uncaught Exception: ${err.message}`);
+        execGracefulShutdown(server);
+    });
+    process.on("unhandledRejection", (reason, promise) => {
+        log.error(
+            `Unhandled Rejection at: ${promise}, reason: ${reason instanceof Error ? reason.message : reason}`
+        );
+        execGracefulShutdown(server);
+    });
+
+export default app;
